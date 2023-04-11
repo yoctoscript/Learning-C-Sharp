bdb1d7782d27f4ed4d5bb7a1f5e88d6eac607c5b
Yoctoscript <yoctoscript@gmail.com>
2023-04-11 22:29:44 +0100

    Hello World

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyNameSpace
+{
+    class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
